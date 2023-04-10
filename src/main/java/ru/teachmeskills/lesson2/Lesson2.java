face10ae1e12d12b50023a045bf2fca48f1e4c07
Радевич Никита Алексндрович (4052457) <vtb4052457@region.vtb.ru>
2023-04-10 17:29:11 +0300

    added lesson 2

--- src/main/java/ru/teachmeskills/lesson2/Lesson2.java
+++ src/main/java/ru/teachmeskills/lesson2/Lesson2.java
@@ -1,7 +1,21 @@
 package ru.teachmeskills.lesson2;
 
+import java.util.Scanner;
+
 public class Lesson2 {
+
+    static final String text = "Text";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double variable = 5.9;
+        int x = 4;
+        double v = 5.0 / 2;
+        System.out.println(5.0 / 2);
+        System.out.println(x);
+        System.out.println(variable);
+        System.out.println("Введите слово, хотите вывести в консоли");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        System.out.print("Hello" + 5);
     }
 }
